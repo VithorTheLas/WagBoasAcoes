21f63e663316d7f6a6d2f270ff8b10604891f049
Vithor Araujo dos Santos <68874318+VithorTheLas@users.noreply.github.com>
2024-10-09 16:00:18 -0300

    refact: remove unneeded div

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -17,39 +17,37 @@ const Checkbox: React.FC<CheckboxProps> = ({
   };
 
   return (
-    <label className="flex items-center cursor-pointer">
-      <div className="relative">
-        <input
-          type="checkbox"
-          className="sr-only"
-          required={required}
-          checked={isChecked}
-          onChange={handleToggle}
-        />
-        <div
-          className={`w-8 h-8 border border-white rounded-lg transition-colors duration-200 ease-in-out ${
-            isChecked
-              ? "bg-blue-500 border-blue-500"
-              : "bg-custom-blue border-gray-300"
-          }`}
-        >
-          {isChecked && (
-            <svg
-              className="w-4 h-4 text-white absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 13l4 4L19 7"
-              ></path>
-            </svg>
-          )}
-        </div>
+    <label className="relative flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        className="sr-only"
+        required={required}
+        checked={isChecked}
+        onChange={handleToggle}
+      />
+      <div
+        className={`w-8 h-8 border border-white rounded-lg transition-colors duration-200 ease-in-out ${
+          isChecked
+            ? "bg-blue-500 border-blue-500"
+            : "bg-custom-blue border-gray-300"
+        }`}
+      >
+        {isChecked && (
+          <svg
+            className="w-4 h-4 text-white absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 13l4 4L19 7"
+            ></path>
+          </svg>
+        )}
       </div>
     </label>
   );
