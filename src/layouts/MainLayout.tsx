4bb0bdb3541a4dc275c519a87e03cc0a8c0f752a
Vithor Araujo dos Santos <68874318+VithorTheLas@users.noreply.github.com>
2024-10-15 15:37:29 -0300

    refact: adapt Main Layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
-import BackgroundHandler from "../components/BackgroundHandler";
 
 const MainLayout: React.FC = () => {
   return (
-    <BackgroundHandler>
+    <>
       <NavBar />
       <Outlet />
-    </BackgroundHandler>
+    </>
   );
 };
 
