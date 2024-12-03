f9475b08ae91787982dc4e8c5c82b9dcb0056276
Vithor Araujo dos Santos <68874318+VithorTheLas@users.noreply.github.com>
2024-12-03 11:05:22 -0300

    feat: add default toast container tag on main file

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
+import "react-toastify/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 const MainLayout: React.FC = () => {
   return (
     <>
       <NavBar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 };
